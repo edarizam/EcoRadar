366656caf883c3945e66c09832dc7ca879dccade
edarizam <edarizam@unal.edu.co>
2025-03-22 15:39:00 -0500

    Crea la capa de services del endpoint faltante

--- src/main/java/com/talentotech2/ecoradar/services/ConsumptionServices.java
+++ src/main/java/com/talentotech2/ecoradar/services/ConsumptionServices.java
@@ -1,6 +1,7 @@
 package com.talentotech2.ecoradar.services;
 
 import com.talentotech2.ecoradar.dto.DefaultDataDTO;
+import com.talentotech2.ecoradar.dto.YearDataDTO;
 import com.talentotech2.ecoradar.repositories.ConsumptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -19,5 +20,9 @@ public class ConsumptionServices {
 
     public List<DefaultDataDTO> findTop10ConsumptionByYearAndSource(int year, Pageable pageable) {
         return consumptionRepository.findTop10ConsumptionsByYear(year, pageable);
+    }
+
+    public List<YearDataDTO> findYearsByLocationId(int year) {
+        return consumptionRepository.findYearsAvailableByLocation(year);
     }
 }
--- src/main/java/com/talentotech2/ecoradar/services/SolarCapacityService.java
+++ src/main/java/com/talentotech2/ecoradar/services/SolarCapacityService.java
@@ -1,6 +1,7 @@
 package com.talentotech2.ecoradar.services;
 
 import com.talentotech2.ecoradar.dto.PercentageDataDTO;
+import com.talentotech2.ecoradar.dto.YearDataDTO;
 import com.talentotech2.ecoradar.repositories.SolarCapacityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -27,5 +28,9 @@ public class SolarCapacityService {
 
     public List<PercentageDataDTO> findTop10SolarCapacityByYear(int year) {
         return  solarCapacityRepository.findTop10SolarCapacityByYear(year, pageable);
+    }
+
+    public List<YearDataDTO> findYearsByLocationId(int year) {
+        return solarCapacityRepository.findYearsAvailableByLocation(year);
     }
 }
