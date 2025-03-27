a627dc60a596866fcf2dfe33774be87fd311f57e
edarizam <edarizam@unal.edu.co>
2025-03-27 02:48:19 -0500

    Elimina archivos no usados

--- src/main/java/com/talentotech2/ecoradar/controllers/PageController.java
+++ src/main/java/com/talentotech2/ecoradar/controllers/PageController.java
@@ -4,31 +4,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
     @GetMapping("/")
     public String home() {
-        return "index"; // Esto busca "index.html" en la carpeta "templates"
+        return "index";
     }
 
     @GetMapping("/contacto")
     public String contacto() {
         return "contacto";
     }
+
     @GetMapping("/formularioinfo")
     public String formularioinfo() {
         return "formularioinfo";
     }
+
     @GetMapping("/rankings")
     public String rankings() {
         return "rankings";
     }
+
     @GetMapping("/iniciarsesion")
     public String iniciarsesion() {
         return "iniciarsesion";
     }
+
     @GetMapping("/Registro")
     public String Registro() {
         return "Registro";
     }
+
     @GetMapping("/informacion")
     public String informacion() {
         return "informacion";
@@ -36,7 +42,7 @@ public class PageController {
     
     @GetMapping("/coming-soon")
     public String comingSoonPage() {
-        return "coming-soon"; // Thymeleaf renderizará `coming-soon.html`
+        return "coming-soon";
     }
 }
 
